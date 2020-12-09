85887765864430c15fb1e20cb4f1054f3a438953
aalvarezbretana <aalvarezvbretana>
2020-12-09 14:05:09 +0100

    Un poco más cerca...

--- app/src/main/java/com/dam2/simondice/MainActivity.kt
+++ app/src/main/java/com/dam2/simondice/MainActivity.kt
@@ -10,22 +10,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    var random:Int = (0..3).random()
-    val fourColors:Array<String> = arrayOf("Green", "Yellow", "Blue", "Red")
-    val allColors:ArrayList<String> = arrayListOf(fourColors[random])
-    val start:Button = findViewById<Button>(R.id.startButton)
-    val activitiesArray:Array<Class<Verde>> = arrayOf(Verde::class.java)
+        var random: Int = (0..3).random()
+        val fourColors: Array<String> = arrayOf("Green", "Yellow", "Blue", "Red")
+        val allColors: ArrayList<String> = arrayListOf(fourColors[random])
+        val start: Button = findViewById<Button>(R.id.startButton)
+        val activitiesArray: Array<Class<Verde>> = arrayOf(Verde::class.java)
 
-    for(i:Int in 0..3){
-        random = (0..3).random()
-        allColors.add(fourColors[random])
-    }
+        for (i: Int in 0..3) {
+            random = (0..3).random()
+            allColors.add(fourColors[random])
+        }
 
-    start.setOnClickListener {
-        val intent = Intent(this@MainActivity, Verde::class.java)
-        intent.putStringArrayListExtra("colors",allColors)
-    }
-
-
+        start.setOnClickListener {
+            val intent = Intent(this@MainActivity, Verde::class.java)
+            intent.putStringArrayListExtra("colors", allColors)
+            intent.putExtra("count", 0)
+            intent.putExtra("score", 0)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/dam2/simondice/Verde.kt
+++ app/src/main/java/com/dam2/simondice/Verde.kt
@@ -1,11 +1,87 @@
 package com.dam2.simondice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class Verde : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verde)
+
+        val title = findViewById<TextView>(R.id.titleText)
+        val scoreText = findViewById<TextView>(R.id.scoreText)
+        val green = findViewById<Button>(R.id.greenButton)
+        val yellow = findViewById<Button>(R.id.yellowButton)
+        val blue = findViewById<Button>(R.id.blueButton)
+        val red = findViewById<Button>(R.id.redButton)
+        val restart = findViewById<Button>(R.id.restartButton)
+        val activitiesArray = arrayOf(MainActivity::class.java, Verde::class.java)
+
+
+        var score = intent.getIntExtra("score", -2)
+        var count = intent.getIntExtra("count", -3)
+        val colors: ArrayList<String> = intent.getStringArrayListExtra("colors") as ArrayList<String>
+
+
+        scoreText.text = score.toString()
+
+
+        if (score != count) {
+            val temp = "Color: " + (count + 1)
+            title.text = temp
+        } else {
+            val temp = "Simon dice " + colors[count]
+            title.text = temp
+        }
+
+
+        fun gameOver(newTitle: String) {
+            colors[count] = newTitle
+            title.text = newTitle
+            restart.visibility = View.VISIBLE
+            red.text = newTitle
+            yellow.text = newTitle
+            green.text = newTitle
+            blue.text = newTitle
+        }
+
+
+        fun onCorrect(answer: String, classNum: Int) {
+            if (colors[count] == answer) {
+                val intent = Intent(this@Verde, activitiesArray[classNum])
+                if ((count + 1) == colors.size) {
+                    gameOver("GANASTE")
+                } else {
+                    if (count == score) {
+                        count--
+                        score++
+                    }
+                    count++
+                    intent.putStringArrayListExtra("colors", colors)
+                    intent.putExtra("count", 0)
+                    intent.putExtra("score", 0)
+                    startActivity(intent)
+                }
+            } else if (restart.visibility != 0) {
+                gameOver("gameOver")
+            }
+        }
+
+        green.setOnClickListener {
+            onCorrect("Green", 0)
+        }
+        yellow.setOnClickListener {
+            onCorrect("Yellow", 0)
+        }
+        blue.setOnClickListener {
+            onCorrect("Blue", 0)
+        }
+        red.setOnClickListener {
+            onCorrect("Red", 0)
+        }
     }
 }
